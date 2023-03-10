1464fbde779e9a874751c8ef7e24c3df09db5cbf
John Astill <john.astill@sap.com>
2023-03-10 10:26:10 -0500

    Initial commit with basic setup

---
+++ init.lua
@@ -0,0 +1,119 @@
+local vim = vim
+local api = vim.api
+
+-- needed by nvim-compe
+vim.o.completeopt = "menuone,noselect"
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.syntax = 'on'
+vim.o.smarttab = true
+-- No case sensitive searches
+vim.o.ignorecase = true
+-- If upper case is entered, use upper case
+vim.o.smartcase = true
+
+-- i sometimes use the mouse to scroll through a buffer
+vim.cmd [[set mouse=a]]
+-- otherwise vim replaces the content of current buffer with the new file you
+-- open. Or maybe deletes the current buffer and creates a new one. Either way,
+-- it makes swithcing between open files quickly a pain in the ass.
+-- If i set the hidden option, i lose the line numbers for some reason. Not
+-- for every file though. If i open this file, everything's fine. If i open
+-- a directory and then open a js file. Boom!
+vim.o.hidden = true
+vim.cmd [[set report=2]]
+-- took me a long time to figure out how to change the leader key in lua
+vim.g.mapleader = " "
+
+local key_mapper = function(mode, key, result)
+  vim.api.nvim_set_keymap(
+    mode,
+    key,
+    result,
+    {noremap = true, silent = true}
+  )
+end
+
+key_mapper('n', 'gd', ':lua vim.lsp.buf.definition()<CR>')
+key_mapper('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>')
+key_mapper('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>')
+key_mapper('n', 'gw', ':lua vim.lsp.buf.document_symbol()<CR>')
+key_mapper('n', 'gW', ':lua vim.lsp.buf.workspace_symbol()<CR>')
+key_mapper('n', 'gr', ':lua vim.lsp.buf.references()<CR>')
+key_mapper('n', 'gt', ':lua vim.lsp.buf.type_definition()<CR>')
+key_mapper('n', 'K', ':lua vim.lsp.buf.hover()<CR>')
+key_mapper('n', '<c-k>', ':lua vim.lsp.buf.signature_help()<CR>')
+key_mapper('n', '<leader>af', ':lua vim.lsp.buf.code_action()<CR>')
+key_mapper('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
+
+key_mapper('n', '<C-p>', ':lua require"telescope.builtin".find_files()<CR>')
+key_mapper('n', '<leader>fs', ':lua require"telescope.builtin".live_grep()<CR>')
+key_mapper('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
+key_mapper('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>')
+
+local execute = vim.api.nvim_command
+local fn = vim.fn
+-- ensure that packer is installed
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+    execute 'packadd packer.nvim'
+end
+vim.cmd('packadd packer.nvim')
+local packer = require'packer'
+local util = require'packer.util'
+packer.init({
+  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
+--- startup and add configure plugins
+packer.startup(function()
+  local use = use
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-lua/telescope.nvim'
+  use 'jremmen/vim-ripgrep'
+  use 'neovim/nvim-lspconfig'
+  use 'nvim-lua/completion-nvim'
+  use 'anott03/nvim-lspinstall'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'sheerun/vim-polyglot'
+  -- these are optional themes but I hear good things about gloombuddy ;)
+  -- colorbuddy allows us to run the gloombuddy theme
+  use 'tjdevries/colorbuddy.nvim'
+  use 'bkegley/gloombuddy'
+  -- sneaking some formatting in here too
+  use {'prettier/vim-prettier', run = 'yarn install' }
+  end
+)
+
+ --vim.g.colors_name = 'gloombuddy'
+
+local configs = require'nvim-treesitter.configs'
+configs.setup {
+  --ensure_installed = "maintained",
+  highlight = {
+    enable = true,
+  }
+}
+
+local lspconfig = require'lspconfig'
+local completion = require'completion'
+local function custom_on_attach(client)
+  print('Attaching to ' .. client.name)
+  completion.on_attach(client)
+end
+local default_config = {
+  on_attach = custom_on_attach,
+}
+-- setup language servers here
+lspconfig.tsserver.setup(default_config)
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+  }
+)
