31d71656216a703dd603999ed931c7cf8b188c20
John Astill <john.astill@sap.com>
2023-03-10 12:04:56 -0500

    Broken color scheme, but more cleanup

--- init.lua
+++ init.lua
@@ -6,24 +6,27 @@ vim.o.completeopt = "menuone,noselect"
 
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
+vim.o.et = true
 vim.o.syntax = 'on'
 vim.o.smarttab = true
+vim.o.number = true
+
 -- No case sensitive searches
 vim.o.ignorecase = true
+
 -- If upper case is entered, use upper case
 vim.o.smartcase = true
 
--- i sometimes use the mouse to scroll through a buffer
 vim.cmd [[set mouse=a]]
--- otherwise vim replaces the content of current buffer with the new file you
--- open. Or maybe deletes the current buffer and creates a new one. Either way,
--- it makes swithcing between open files quickly a pain in the ass.
--- If i set the hidden option, i lose the line numbers for some reason. Not
--- for every file though. If i open this file, everything's fine. If i open
--- a directory and then open a js file. Boom!
+
 vim.o.hidden = true
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+-- set the status line
 vim.cmd [[set report=2]]
--- took me a long time to figure out how to change the leader key in lua
+
+-- set the leader key
 vim.g.mapleader = " "
 
 local key_mapper = function(mode, key, result)
@@ -34,6 +37,9 @@ local key_mapper = function(mode, key, result)
     {noremap = true, silent = true}
   )
 end
+
+-- Easily open this file with a keymap as I am in here a lot
+key_mapper('n', '<leader>v', ':edit $MYVIMRC<CR>')
 
 key_mapper('n', 'gd', ':lua vim.lsp.buf.definition()<CR>')
 key_mapper('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>')
@@ -79,15 +85,17 @@ packer.startup(function()
   use 'nvim-treesitter/nvim-treesitter'
   use 'sheerun/vim-polyglot'
   -- these are optional themes but I hear good things about gloombuddy ;)
-  -- colorbuddy allows us to run the gloombuddy theme
-  use 'tjdevries/colorbuddy.nvim'
-  use 'bkegley/gloombuddy'
+  use "EdenEast/terafox.nvim"
+  use "EdenEast/teratox.nvim"
+  
+  use 'folke/tokyonight.nvim'
   -- sneaking some formatting in here too
   use {'prettier/vim-prettier', run = 'yarn install' }
   end
 )
 
- --vim.g.colors_name = 'gloombuddy'
+--vim.o.termguicolors = true
+--vim.cmd("colorscheme nightfox")
 
 local configs = require'nvim-treesitter.configs'
 configs.setup {
@@ -117,3 +125,6 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     update_in_insert = true,
   }
 )
+
+vim.api.nvim_create_user_command('ReloadConfig', 'source $MYVIMRC', {})
+
